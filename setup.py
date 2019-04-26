34817069f625dc0d52005c46ed256e7c71d68838
coxg <gavencox@gmail.com>
2019-04-25 23:37:52 -0400

    Adding a long description

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='data_tools',
-    version='0.2',
+    version='0.21',
     description='Tools for data engineers',
     url='https://github.com/coxg/data_tools',
     author='Gaven Cox',
     author_email='gavencox@gmail.com',
     license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['data_tools']
 )
